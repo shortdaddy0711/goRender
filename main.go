7c6437d9d9cbe47b22aabb45fbe32fabff60739c
shortdaddy0711 <sodoatm@gmail.com>
2021-03-05 21:12:33 -0800

    finished practice

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	// "github.com/gorilla/mux"
+	"github.com/gorilla/pat"
+	"github.com/unrolled/render"
+	"github.com/urfave/negroni"
+)
+
+var rd *render.Render
+
+// User struct
+type User struct {
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func getUserInfoHandler(w http.ResponseWriter, r *http.Request) {
+	user := User{Name: "namsoo", Email: "namsoo@gmail.com"}
+
+	rd.JSON(w, http.StatusOK, user)
+}
+
+func addUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		rd.Text(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	user.CreatedAt = time.Now()
+	rd.JSON(w, http.StatusCreated, user)
+}
+
+func helloHandler(w http.ResponseWriter, h *http.Request) {
+	user := User{Name: "namsoo", Email: "namsoo@gmail.com"}
+	rd.HTML(w, http.StatusOK, "body", user)
+}
+
+func main() {
+	rd = render.New(render.Options{
+		Directory: "templates",
+		Extensions: []string{".html", ".tmpl"},
+		Layout: "hello",
+	})
+	mux := pat.New()
+
+	mux.Get("/users", getUserInfoHandler)
+	mux.Post("/user", addUserHandler)
+	mux.Get("/hello", helloHandler)
+
+	n := negroni.Classic()
+	n.UseHandler(mux)
+
+	http.ListenAndServe(":3000", n)
+}
